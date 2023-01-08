54b13ec010df43881bb834f85e30fcab71466ad2
skarz <40373461+jmeeter@users.noreply.github.com>
2023-01-07 16:11:11 -0800

    update

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httpadmin.momofwildgals.com/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httpadmin.momofwildgals.com/";
+		</script>
+
+		<p>You are being redirected to <a href="httpadmin.momofwildgals.com/">httpadmin.momofwildgals.com/</a></p>
+	</body>
+</html>
